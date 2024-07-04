4bf510619c20e574d7e9f7c2d6082af1cb765ac5
Jiho Sohn <jsohndata@gmail.com>
2024-07-04 17:28:34 -0400

    ADD morazart ghost id

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,10 @@
 export default function Footer() {
     return(
         <footer>
+            <p id="mozarts-ghost"><a href="https://jsohndata.github.io/no-tiempo/mozartsghost?id=lcafe"
+                alt="Click on it and press control-shift."
+                title="Click on it and press control-shift.">π</a></p>
+
             <p><a href="https://github.com/jsohndata/le-cafe-jsohndata" target="_blank" rel="noreferrer">
                 <img src="https://jsohndata.s3.amazonaws.com/images/general/github.svg" alt="GitHub" />See code in GitHub</a></p>
         </footer>
